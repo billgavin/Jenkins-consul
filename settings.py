3269a15838e5aaa250409090d53f7425a6b1c966
wunaijiang <wunaijiang@fang.com>
2018-05-28 17:08:43 +0800

    修改路径设置

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ hostname, ip = get_local_ip(ifname)
 TOKEN_APP = 'ccs'
 TOKEN_KEY = 'fang.com'
 API_SERVER = 'http://ccs.light.fang.com/api/v1/'
-JENKINS_URL = 'http://%s/download/' % ip
+JENKINS_URL = 'http://%s/download' % ip
 CMDB = 'http://ops.light.fang.com/api/check/ip/?ip='
 SALT_CHECK = 'http://saltnew.light.soufun.com/ping/'
 USER_NAME= 'jenkins'
